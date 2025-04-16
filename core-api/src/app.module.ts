614798691947492eac539cdedbbb223e2230eaaa
김정호 <jeangho293@naver.com>
2025-04-16 12:54:38 +0900

    [feat] swagger 기초 구성

--- core-api/src/app.module.ts
+++ core-api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigsModule } from '@configs';
 import { DatabasesModule } from './databases';
 import { PatientsModule } from './services/patients/patients.module';
+import { SwaggerModule } from '@libs/swagger';
 
 @Module({
-  imports: [ConfigsModule, DatabasesModule, PatientsModule],
+  imports: [ConfigsModule, DatabasesModule, SwaggerModule, PatientsModule],
   controllers: [],
   providers: [],
 })
