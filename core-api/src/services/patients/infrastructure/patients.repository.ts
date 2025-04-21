fb58e4142b7bfba5338a75c2b00674345ee050c5
김정호 <jeangho293@naver.com>
2025-04-21 12:20:41 +0900

    [fix] 이거 안될듯..

--- core-api/src/services/patients/infrastructure/patients.repository.ts
+++ core-api/src/services/patients/infrastructure/patients.repository.ts
@@ -53,35 +53,31 @@ export class PatientsRepository extends DddRepository<Patient> {
         .values(patients)
         .execute();
 
-      //       // step 3: chart 있는 데이터가 chart 없는 기존 레코드를 업데이트 (빈 값은 덮어쓰지 않음)
-      //       await queryRunner.query(`
-      //   UPDATE patient p
-      //   JOIN temp_patient_import t
-      //     ON p.chart = '' AND t.chart != ''
-      //     AND p.name = t.name AND p.phone = t.phone
-      //   SET
-      //     p.chart = t.chart,
-      //     p.name = COALESCE(NULLIF(t.name, ''), p.name),
-      //     p.address = COALESCE(NULLIF(t.address, ''), p.address),
-      //     p.memo = COALESCE(NULLIF(t.memo, ''), p.memo)
-      // `);
+      // step 3: chart 있는 데이터가 chart 없는 기존 레코드를 업데이트 (빈 값은 덮어쓰지 않음)
+      await queryRunner.query(`
+        UPDATE patient p
+        JOIN temp_patient_import t
+          ON p.chart = '' AND t.chart != ''
+          AND p.name = t.name AND p.phone = t.phone
+        SET
+          p.chart = t.chart,
+          p.address = t.address,
+          p.rrn = t.rrn
+          p.memo = t.memo
+      `);
 
-      //       // step 4: chart 없는 데이터가 chart 있는 기존 레코드를 업데이트 (chart는 덮어쓰지 않음)
-      //       await queryRunner.query(`
-      // -- step 4: chart 없는 데이터를 chart 있는 기존 레코드에 병합 (값이 있는 것만 덮어쓰기)
-      // UPDATE patient p
-      // JOIN temp_patient_import t
-      //   ON p.chart != '' AND t.chart = ''
-      //   AND p.name = t.name AND p.phone = t.phone
-      // SET
-      //   p.name = t.name,
-      //   p.address = IFNULL(NULLIF(t.address, ''), p.address),
-      //   p.memo = IFNULL(NULLIF(t.memo, ''), p.memo)
+      // step 4: chart 없는 데이터가 chart 있는 기존 레코드를 업데이트 (chart는 덮어쓰지 않음)
+      await queryRunner.query(`
+      UPDATE patient p
+      JOIN temp_patient_import t
+        ON p.chart != '' AND t.chart = ''
+        AND p.name = t.name AND p.phone = t.phone
+      SET
 
-      // `);
+      `);
 
-      //       // step 5: chart 없는 데이터 중 기존에 없던 환자 insert
-      //       await queryRunner.query(`
+      // // step 5: chart 없는 데이터 중 기존에 없던 환자 insert
+      // await queryRunner.query(`
       // INSERT INTO patient (chart, name, phone, rrn, address, memo)
       // SELECT t.chart, t.name, t.phone, t.rrn, t.address, t.memo
       // FROM temp_patient_import t
